6aeeab2add6f0423d64b0e85a88bf87b418e2c28
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-20 20:18:57 -0300

    cambios de adaptacion a version vieja de spring boot

--- src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
+++ src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
@@ -1,8 +1,11 @@
 package com.formacionbdi.springboot.app.productos;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootServicioProductosApplicationTests {
 
