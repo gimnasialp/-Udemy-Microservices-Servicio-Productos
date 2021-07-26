c4b38a6dcb9f1e2f7b0047d087b1cf2a32fed0d3
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-25 22:23:08 -0300

    26. Configurando timeout en Hystrix y Ribbon - recordar que ribbon provee 1 seg de timeout

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -36,6 +36,12 @@ public class ProductoController {
         Producto producto = productoService.findById(id);
         //producto.setPort(Integer.parseInt(env.getProperty("server.port")));
         producto.setPort(port);
+
+        /*try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         return  producto;
     }
 
