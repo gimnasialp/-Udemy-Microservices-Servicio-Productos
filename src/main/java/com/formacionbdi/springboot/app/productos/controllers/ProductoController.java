0fc6991e4ce55e2513d1b5f03aa5e9b09af3a40f
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-25 22:03:17 -0300

    25 - Tolerancia a fallos y latencia con Hystrix 2- Circuit Breaker Pattern

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.formacionbdi.springboot.app.productos.controllers;
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,9 @@ public class ProductoController {
     @Autowired
     private Environment env;
 
+    @Value("${server.port}")
+    private Integer port;
+
     @Autowired
     private IProductoService productoService;
 
@@ -30,7 +34,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable  Long id){
         Producto producto = productoService.findById(id);
-        producto.setPort(Integer.parseInt(env.getProperty("server.port")));
+        //producto.setPort(Integer.parseInt(env.getProperty("server.port")));
+        producto.setPort(port);
         return  producto;
     }
 
